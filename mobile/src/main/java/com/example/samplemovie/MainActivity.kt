ae9323e59873b6c6720199d8c7542fdc76ecbadf
haiminhtran810 <haiminhtran810@gmail.com>
2019-05-10 11:37:57 +0700

    init

---
+++ mobile/src/main/java/com/example/samplemovie/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.samplemovie
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                textMessage.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                textMessage.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        MobileAds.initialize(this, "ca-app-pub-3524852624424593~5281515290")
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+}
